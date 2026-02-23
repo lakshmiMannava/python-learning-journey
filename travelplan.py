5816fa953da569b8d2b879f9499f5cb128930814
lakshmiMannava <mannavalakshmi95@gmail.com>
2026-02-23 16:39:50 +0530

    Add files via upload

---
+++ travelplan.py
@@ -0,0 +1,29 @@
+** start of main.py **
+
+distance_mi = 1
+is_raining = True
+has_bike = False
+has_car = True
+has_ride_share_app = True
+
+if not distance_mi :
+    print(False)
+elif distance_mi <= 1 :
+    if not is_raining :
+        print(True)
+    else :
+        print(False)
+elif 1 < distance_mi <= 6 :
+    if has_bike and not is_raining :
+        print(True)
+    else :
+        print(False)
+elif distance_mi > 6 :
+    if has_car or has_ride_share_app :
+        print(True)
+    else :
+        print(False)
+
+
+** end of main.py **
+
